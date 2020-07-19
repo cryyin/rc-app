96150765f4fad64043895b74e020ef62d8be6bb3
mylinlan <ljx20132200022@gmail.com>
2020-07-19 17:35:44 +0800

    + feat: data

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,21 +1,30 @@
 import React, {useState, useEffect} from 'react';
-import {List} from "antd"
+import {List, Typography} from "antd"
 import request from '@/utils/request';
 
 const TableView = prop =>  {
-    const [list, setList] = useState([])
-    useEffect(()=>{
-        request.get('/role').then(r=>{
-            console.log(r)
-            setList(r.dataList)
-        })
-    })
+    const [list, setList] = useState([]);
+    useEffect( ()=>{
+        const fetchData = async () => {
+            const result = await  request.get('/role');
+            setList(result.data.dataList);
+        };
+        // noinspection JSIgnoredPromiseFromCall
+        fetchData();
+    },[]);
+
     return (
         <div>
             <div>
+                {list &&list[0] && list[0]['roleDesc']}
                 <List
                     size="small"
                     dataSource={list}
+                    renderItem={item => (
+                        <List.Item>
+                            {item.id}
+                        </List.Item>
+                    )}
                 />
             </div>
         </div>
