cf649564c85cf13cc45cb982c0b3e06826039223
mylinlan <ljx20132200022@gmail.com>
2020-07-20 21:25:43 +0800

    * fix: demo echarts 数据

--- src/views/charts/chartData.js
+++ src/views/charts/chartData.js
@@ -18,4 +18,132 @@ const ChartData = {
     }
 }
 
+// noinspection ES6ModulesDependencies
+export const getBarOptions = (barData) => {
+    return {
+        color: ['rgba(0, 241, 244, 1)', 'rgba(0, 134, 244, 1)'],
+        legend: {
+            data: barData.legend,
+            bottom: 5
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        grid: {
+            left: '3%',
+            right: '3%',
+            top: '5%',
+            bottom: '20%',
+            containLabel: true
+        },
+        yAxis: [
+            {
+                type: 'value',
+                axisLine: {show: false, lineStyle: {color: '#B5B2B5',}},
+                axisLabel: {show: true, textStyle: {color: "#666666", fontSize: 12},},
+                splitLine: {show: true, lineStyle: {color: "#B5B2B5", type: 'dashed'}},
+                axisTick: {show: false},
+            }
+        ],
+        xAxis: [
+            {
+                type: 'category',
+                data: barData.axis,
+                boundaryGap: true,
+                axisPointer: {type: 'shadow'},
+                axisLine: {lineStyle: {color: '#B5B2B5'}},
+                splitLine: {show: false, lineStyle: {color: "#B5B2B5"}},
+                axisLabel: {
+                    interval: 0,
+                    textStyle: {color: "#666666", fontSize: 12},
+                },
+                axisTick: {show: true, alignWithLabel: true},
+            }
+        ],
+        series: [
+            {
+                name: barData.legend[0],
+                type: 'bar',
+                stack: 'two',
+                barWidth: "20%",
+                data: barData.data1
+            },
+            {
+                name: barData.legend[1],
+                type: 'bar',
+                stack: 'two',
+                barWidth: "20%",
+                data: barData.data2
+            }
+
+        ]
+    };
+}
+
+export const getLineOptions = lineData => {
+    let series = [];
+    for (let i = 0; i < lineData.legend.length; i++) {
+        series.push({
+            name: lineData.legend[i],
+            type: 'line',
+//          smooth: true,
+            symbol: 'circle',
+            stack: '总量',
+            data: lineData.data[i]
+        });
+    }
+    return  {
+        grid: {
+            left: '5%',
+            right: '5%',
+            top: '5%',
+            bottom: '15%',
+            containLabel: true
+        },
+        legend: {
+            data: lineData.legend,
+            bottom: 5
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        color: ['#0067FF', '#FFA718', 'rgba(181,61,14,1)'],
+        xAxis: [
+            {
+                type: 'category',
+                boundaryGap: true,
+                axisLine: {lineStyle: {color: '#B5B2B5'}},
+                axisTick: {show: true, alignWithLabel: true},
+                splitLine: {show: false},
+                axisLabel: {
+                    interval: 0,
+                    textStyle: {color: "#666666", fontSize: '12'},
+                    rotate: 40,
+                },
+                data: lineData.axis
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value',
+                axisTick: {show: false},
+                splitLine: {show: true, lineStyle: {type: 'dashed'}},
+                axisLabel: {
+                    show: true,
+                    color: "#666666",
+                    textStyle: {
+                        fontSize: '12'
+                    }
+                },
+                axisLine: {
+                    show: false,
+                    lineStyle: {
+                        color: "#666666",
+                    }
+                },
+            }
+        ],
+        series: series
+    };
+}
 export default ChartData;
--- src/views/charts/index.jsx
+++ src/views/charts/index.jsx
@@ -1,7 +1,24 @@
 import React, {useState} from "react";
-import DefaultChartData from './chartData';
+import { Row, Col } from 'antd';
+import DefaultChartData, {getBarOptions, getLineOptions} from './chartData';
 import './index.less'
 
+// 全部引入
+// import ReactEcharts from "echarts-for-react";
+// 按需引入减少包体积
+import ReactEchartsCore from 'echarts-for-react/lib/core';
+import echarts from 'echarts/lib/echarts';
+import 'echarts/lib/chart/bar';
+import 'echarts/lib/component/title';
+import 'echarts/lib/chart/line';
+import 'echarts/lib/component/tooltip';
+
+/**
+ * ReactEcharts使用
+ * @see "https://github.com/hustcc/echarts-for-react"
+ * @returns {*}
+ * @constructor
+ */
 const Visualization = () => {
     const [chartData, setChartData] = useState(
         {...DefaultChartData}
@@ -10,56 +27,69 @@ const Visualization = () => {
         <div className='chart-container'>
             <div className="container">
                 <span className="page-title">客户A关于外运公司A的可视化报告</span>
-                <div className="row">
-                    <div className="col-md-3 col-sm-3">
+                <Row >
+                    <Col span={6}>
                         <div className="d_block db01">
                             <span className="txt">近3个月累计收入(万元)</span>
                             <span className="num">37435.4</span>
                         </div>
-                    </div>
-                    <div className="col-md-3 col-sm-3">
+                    </Col>
+                    <Col span={6}>
                         <div className="d_block db02">
                             <span className="txt">近3个月平均应收账款(万元)</span>
                             <span className="num">37435.4</span>
                         </div>
-                    </div>
-                    <div className="col-md-3 col-sm-3">
+                    </Col>
+                    <Col span={6}>
                         <div className="d_block db03">
                             <span className="txt">授信期</span>
                             <span className="num">37435.4</span>
                         </div>
-                    </div>
-                    <div className="col-md-3 col-sm-3">
+                    </Col>
+                    <Col span={6}>
                         <div className="d_block db04">
                             <span className="txt">授信额(万元)</span>
                             <span className="num">37435.4</span>
                         </div>
-                    </div>
-                    <div className="col-md-6 col-sm-6">
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={12}>
                         <div className="conbox m_t15">
                             <div className="conbox_top">
                                 <label>应收账款与收入变化趋势图</label>
                             </div>
-                            <div className="ech_box01" id="ech_a"/>
+                            <ReactEchartsCore
+                                echarts={echarts}
+                                option={getLineOptions(chartData.A)}
+                            />
                         </div>
-                    </div>
-                    <div className="col-md-6 col-sm-6">
+                    </Col>
+                    <Col span={12}>
                         <div className="conbox m_t15">
                             <div className="conbox_top">
                                 <label>应收账款周转率趋势图</label>
                             </div>
-                            <div className="ech_box01" id="ech_b"/>
+                            <ReactEchartsCore
+                                echarts={echarts}
+                                option={getLineOptions(chartData.B)}
+                            />
                         </div>
-                    </div>
-                    <div className="col-md-12 col-sm-12">
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={24}>
                         <div className="conbox">
                             <div className="conbox_top">
                                 <label>应收账款分账龄分布情况</label>
                             </div>
-                            <div className="ech_box02" id="ech_c"/>
+                            <ReactEchartsCore
+                                echarts={echarts}
+                                option={getBarOptions(chartData.C)}
+                            />
                         </div>
-                    </div>
-                </div>
+                    </Col>
+                </Row>
             </div>
         </div>
     )
