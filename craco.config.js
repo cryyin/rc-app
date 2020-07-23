2e202b7d03765b71488ce01bcd44157d28a3abb5
mylinlan <ljx20132200022@gmail.com>
2020-07-24 00:28:16 +0800

    - clean:

--- craco.config.js
+++ craco.config.js
@@ -1,38 +1,21 @@
 const CracoLessPlugin = require('craco-less');
 // craco配置 "https://github.com/gsoft-inc/craco/blob/master/packages/craco/README.md#custom-location-for-cracoconfigjs"
 const path = require("path");
+
 function resolve(dir) {
     return path.join(__dirname, dir);
 }
+
 module.exports = {
-    webpack:{
+    webpack: {
         alias: {
             "@": resolve("src"),
         },
-        configure:{
-            module:{
-                rules:[
-                    {
-                        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                        use: [
-                            {
-                                loader: 'babel-loader',
-                            },
-                            {
-                                loader: '@svgr/webpack',
-                                options: {
-                                    babel: false,
-                                    icon: true,
-                                },
-                            },
-                        ],
-                    }
-                ]
-            },
-            output:{
-                publicPath:process.env.NODE_ENV === "production" ? "" : "/"
-            }
+        configure: (config, {env, paths}) => {
+            config.output.publicPath = env.NODE_ENV === "production" ? "" : "/"
+            return config;
         }
+
     },
     plugins: [
         {
@@ -42,7 +25,7 @@ module.exports = {
                     // 可以在这里更改antd主题
                     // @see "https://ant.design/docs/react/use-with-create-react-app-cn"
                     lessOptions: {
-                        modifyVars: { '@primary-color': '#1890ff' },
+                        modifyVars: {'@primary-color': '#1890ff'},
                         javascriptEnabled: true,
                     },
                 },
